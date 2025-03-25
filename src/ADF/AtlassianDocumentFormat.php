3a4b723b83af99e86421c0504f6490fbe27bb12e
Romain MILLAN <rmillan@spiriit.com>
2025-03-25 19:55:11 +0100

    feat: AtlassianDocumentFormat allow string into constructor

--- src/ADF/AtlassianDocumentFormat.php
+++ src/ADF/AtlassianDocumentFormat.php
@@ -18,8 +18,18 @@ class AtlassianDocumentFormat implements \JsonSerializable
 
     private ?Document $document = null;
 
-    public function __construct(Document|Node $document)
+    public function __construct(Document|Node|string $document)
     {
+        if (is_string($document)) {
+            $this->document = (new Document())
+                ->paragraph()
+                ->text($document)
+                ->end()
+            ;
+
+            return;
+        }
+
         $this->document = $document;
     }
 
